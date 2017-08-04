69b56b0415ea4ea3a57eccf22b10b5fe937cb2a1
Jonny Holupchinski <jonholup@gmail.com>
2017-08-04 17:07:34 -0500

    list posts

--- 6 - Using the WP REST API Outside WP/6.1-list-posts/src/js/Posts.js
+++ 6 - Using the WP REST API Outside WP/6.1-list-posts/src/js/Posts.js
@@ -1,51 +1,47 @@
-// Import axios from 'axios';
+import axios from 'axios';
 import config from './config';
 import Helpers from './Helpers';
 
 export default class Posts {
+  static loadPosts(event) {
+    if (event) {
+      event.preventDefault();
+    }
 
-    static loadPosts( event ) {
-
-        if ( event ) {
-            event.preventDefault();
+    // Add 'wp/v2/posts' to the end of config.rest_url
+    // Change TOTAL_POSTS to 3
+    axios
+      .get(config.rest_url + 'wp/v2/posts', {
+        params: {
+          per_page: 3
         }
+      })
+      .then(response => {
+        Helpers.clearPosts();
+        // Change POSTS to response.data
+        for (let post of response.data) {
+          // Change POST_HERE to post
+          Posts.renderPost(post);
+        }
+      });
+  }
 
-        // Add 'wp/v2/posts' to the end of config.rest_url
-        // Change TOTAL_POSTS to 3
-        axios.get( config.rest_url, {
-                params: {
-                    per_page: TOTAL_POSTS
-                }
-            } )
-            .then( response => {
-                Helpers.clearPosts();
-                // Change POSTS to response.data
-                for ( let post of POSTS ) {
-                    // Change POST_HERE to post
-                    Posts.renderPost( POST_HERE );
-                }
-            } );
+  static renderPost(post) {
+    const article = document.createElement('article');
+    let markup = '';
 
-    }
+    // Change SLUG_HERE to post.slug
+    // Change TITLE_HERE to post.title.rendered
+    // Change EXCERPT_HERE to post.excerpt.rendered
+    markup += `<h2 class="entry-title"><a href="#${post.slug}">${post.title
+      .rendered}</a></h2>`;
+    markup += `<div class="entry-content">${post.excerpt.rendered}</div>`;
 
-    static renderPost( post ) {
+    // Change ID_HERE to post.id
+    article.classList.add('post');
+    article.dataset.id = post.id;
+    article.innerHTML = markup;
 
-        const article = document.createElement( 'article' );
-        let markup = '';
-
-        // Change SLUG_HERE to post.slug
-        // Change TITLE_HERE to post.title.rendered
-        // Change EXCERPT_HERE to post.excerpt.rendered
-        markup += `<h2 class="entry-title"><a href="#${SLUG_HERE}">${TITLE_HERE}</a></h2>`;
-        markup += `<div class="entry-content">${EXCERPT_HERE}</div>`;
-
-        // Change ID_HERE to post.id
-        article.classList.add( 'post' );
-        article.dataset.id = ID_HERE;
-        article.innerHTML = markup;
-
-        config.appContainer.append( article );
-
-    }
-
+    config.appContainer.append(article);
+  }
 }
